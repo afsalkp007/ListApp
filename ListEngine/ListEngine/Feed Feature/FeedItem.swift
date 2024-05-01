807f2dd82c20b71fef6086177a9768bf786db3d1
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-05-02 00:36:51 +0400

    set up remote feed loader

---
+++ ListEngine/ListEngine/Feed Feature/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  ListEngine
+//
+//  Created by Afsal on 02/05/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+  let name: String
+  let state: String?
+  let country: String
+  let countryCode: String
+  let webpage: [String]
+}
---
+++ ListEngine/ListEngine/Feed Feature/FeedLoader.swift
@@ -0,0 +1,8 @@
+//
+//  FeedLoader.swift
+//  ListEngine
+//
+//  Created by Afsal on 02/05/2024.
+//
+
+import Foundation
