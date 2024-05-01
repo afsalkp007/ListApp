55eb2d1d25bc8ab3332ebfe429768f11156d730b
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-05-02 00:56:52 +0400

    added different cases for remote feed loader with tests

--- ListEngine/ListEngine/Feed API/RemoteFeedLoader.swift
+++ ListEngine/ListEngine/Feed API/RemoteFeedLoader.swift
@@ -7,21 +7,38 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+  case success(HTTPURLResponse)
+  case failure(Error)
+}
+
 public protocol HTTPClient {
-  func get(from url: URL)
+  func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public class RemoteFeedLoader {
   let client: HTTPClient
   let url: URL
+  
+  public enum Error: Swift.Error {
+    case connectivity
+    case invalidData
+  }
 
   public init(url: URL, client: HTTPClient) {
     self.url = url
     self.client = client
   }
 
-  public func load() {
-    client.get(from: url)
+  public func load(completion: @escaping (Error) -> Void) {
+    client.get(from: url) { result in
+      switch result {
+      case .success:
+        completion(.invalidData)
+      case .failure:
+        completion(.connectivity)
+      }
+    }
   }
 }
 
