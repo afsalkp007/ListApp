1542be5cb627b0cbc2c1944b49d437901cf91bd1
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-05-02 00:21:00 +0400

    Created ListEngine mac framework

---
+++ ListEngine/ListEngine/ListEngine.h
@@ -0,0 +1,18 @@
+//
+//  ListEngine.h
+//  ListEngine
+//
+//  Created by Afsal on 02/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ListEngine.
+FOUNDATION_EXPORT double ListEngineVersionNumber;
+
+//! Project version string for ListEngine.
+FOUNDATION_EXPORT const unsigned char ListEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListEngine/PublicHeader.h>
+
+
