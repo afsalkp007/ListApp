24f654286b1f90c113edd43b1d74df29f386033e
Afsal Mohamed <afsalkp.mohammed@gmail.com>
2024-05-02 02:59:39 +0400

    extract memory leak tracking

---
+++ ListEngine/ListEngineTests/Feed API/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,16 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  ListEngineTests
+//
+//  Created by Afsal on 02/05/2024.
+//
+
+import XCTest
+
+extension XCTestCase {
+  func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+    addTeardownBlock { [weak instance] in
+      XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+    }
+  }
+}
